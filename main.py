d8ec8dd54ba76d3526b30dd96b6aafeff588471a
NelsonR <nelsonhenge@hotmail.com>
2022-03-23 20:20:36 -0500

    update tarea done

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from flask import (
 )
 import unittest
 from app import create_app
-from app.forms import DeleteTodoForm, LoginForm, TodoForm
-from app.firestore_service import delete_todo, get_users, get_todos, put_todo
+from app.forms import DeleteTodoForm, LoginForm, TodoForm, UpdateTodoForm
+from app.firestore_service import delete_todo, get_users, get_todos, put_todo, update_todo
 from flask_login import current_user, login_required
 
 app = create_app()
@@ -52,6 +52,7 @@ def hello():
 
     todo_form = TodoForm()
     delete_form = DeleteTodoForm()
+    update_form = UpdateTodoForm()
 
     context = {
         "user_ip": user_ip,
@@ -59,6 +60,7 @@ def hello():
         "username": username,
         "todo_form": todo_form,
         "delete_form": delete_form,
+        "update_form": update_form,
     }
     if todo_form.validate_on_submit():
         put_todo(user_id=username, description=todo_form.description.data)
@@ -76,5 +78,11 @@ def hello():
 def delete(todo_id):
     user_id = current_user.id
     delete_todo(user_id=user_id, todo_id=todo_id)
-
     return redirect(url_for("hello"))
+
+
+@app.route("/todos/update/<todo_id>/<int:done>", methods=["POST"])
+def update(todo_id, done):
+    user_id=current_user.id
+    update_todo(user_id=user_id,todo_id=todo_id,done=done)
+    return redirect(url_for('hello'))
